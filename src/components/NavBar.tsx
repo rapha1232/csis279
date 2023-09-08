3a7bfd3493b671e8d265fd1015a5e493ce7c7c87
raphy1232 <raphy1232@gmail.com>
2023-09-08 19:25:24 +0300

    new folder structure

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -13,7 +13,7 @@ import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
 import { Link } from "react-router-dom";
-import avatar from "../person.png";
+import avatar from "../assets/person.png";
 
 const pages = ["Products", "Pricing", "Blog"];
 const settings = ["Profile", "Account", "Dashboard", "Logout"];
